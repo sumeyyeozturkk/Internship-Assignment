ba9595f5b50258cf00a845af02a6e17a495da453
Sümeyye Öztürk <sozturk15@posta.pau.edu.tr>
2017-05-03 19:54:08 +0300

    remoed editable=false

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class CreateForm(forms.Form):
-    name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    name = forms.CharField(max_length=255)
     sayfa_sayisi = forms.IntegerField()
     yazar_adi = forms.CharField(max_length=255)
