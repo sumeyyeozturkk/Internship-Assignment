f28ec78a121211297f196e49ff04fcf09a6e0113
Sümeyye Öztürk <sozturk15@posta.pau.edu.tr>
2017-05-03 19:19:16 +0300

    edit.html changes

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class CreateForm(forms.Form):
-    name = forms.CharField(max_length=255)
+    name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'readonly':'readonly'}))
     sayfa_sayisi = forms.IntegerField()
     yazar_adi = forms.CharField(max_length=255)
